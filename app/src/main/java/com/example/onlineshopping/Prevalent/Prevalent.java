a455fa581b2123f97c3b10892117184178d2d9bf
R <asadznu@gmail.com>
2019-04-20 14:47:59 +0600

    first commit

---
+++ app/src/main/java/com/example/onlineshopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.onlineshopping.Prevalent;
+
+import com.example.onlineshopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
